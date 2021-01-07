9843d7efb30c8e773ec3446d330c8278f9dce231
wh0507 <banggu0507@gmail.com>
2021-01-07 12:45:57 +0900

    一覧表示機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,13 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @tasks = Task.new
   end
 
   def edit

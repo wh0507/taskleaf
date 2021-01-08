81a69076ab8d0de0e1c073dc90445a18d22229d5
wh0507 <banggu0507@gmail.com>
2021-01-08 13:01:49 +0900

    削除機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  
+
   def index
     @tasks = Task.all
   end
@@ -26,6 +26,12 @@ class TasksController < ApplicationController
     task = Task.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
   private

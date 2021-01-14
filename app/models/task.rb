8a1e50ca79f046404075e31ea4461e0f5629edcf
wh0507 <banggu0507@gmail.com>
2021-01-15 00:42:22 +0900

    修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ApplicationRecord
-  validates :name, presence: true
+  validates :name, length: { maximum: 30 }, presence: true
+  validate :validate_name_comma
   belongs_to :user
+
+  private
+
+  def validate_name_comma
+    errors.add(:name, 'にカンマを含めることはできません')if name&.include?(',')
+  end
 end

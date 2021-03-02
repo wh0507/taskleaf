d2807cf15616eba1859c596c3992cfe5a8dbeb10
wh0507 <banggu0507@gmail.com>
2021-03-02 10:16:52 +0900

    更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'taskm' && password == '1324'
+      username == 'taskm' && password == 'p1324'
     end
   end
 end
